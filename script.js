20a2388173be9268a4cd21d8e0c26c345309ed5c
ridho <ridloghfry@gmail.com>
2024-01-14 09:17:06 +0700

    update commit

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const checkbox = document.getElementById("checkbox"),
-  toggle = document.querySelector(".toggle"),
-  toggleCircle = toggle.querySelector(".cycle"),
   body = document.querySelector("body");
 
-toggle.addEventListener("click", () => {
-  checkbox.checked = !checkbox.checked;
+checkbox.addEventListener("change", () => {
   if (checkbox.checked) {
-    toggleCircle.classList.add("light");
-    body.style.backgroundColor = "white";
-    toggle.style.backgroundColor = "rgb(33, 33, 33)";
+    body.classList.add("dark");
   } else {
-    toggleCircle.classList.remove("light");
-    body.style.backgroundColor = "rgb(33, 33, 33)";
-    toggle.style.backgroundColor = "white";
+    body.classList.remove("dark");
   }
 });
